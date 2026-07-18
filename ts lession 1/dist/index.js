62b58deb74b723cccbca65fda71248357273444d
Salman472 <mds619820@gmail.com>
2026-07-18 06:37:39 +0600

    typescript learning project initialize

---
+++ ts lession 1/dist/index.js
@@ -0,0 +1,7 @@
+const count = (num) => {
+    return num;
+};
+const a = 45;
+console.log(count(a));
+export {};
+//# sourceMappingURL=index.js.map
---
+++ ts lession 1/src/index.ts
@@ -0,0 +1,6 @@
+const count = (num : number): number =>{
+    return num
+}
+
+const a = 45
+console.log(count(a));
