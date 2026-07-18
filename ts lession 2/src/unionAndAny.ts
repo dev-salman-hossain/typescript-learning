979ab86842c6dc07278414cb3bf181dc0894be0b
Salman472 <mds619820@gmail.com>
2026-07-18 07:01:41 +0600

    feat: implement TypeScript union type examples

---
+++ ts lession 2/src/unionAndAny.ts
@@ -0,0 +1,10 @@
+let food : 'chicken' | 'biriani' | 'kacci' | 'tehari' //its call union
+
+// amra jokon kon akta variable ar modde bole dei je ar man ki hobe. amra age tekei bole dite pari eitai union ar kaj
+// for example
+
+let personDetails : string | number //amra eikane bole dicci je person details a string and number use korte parbe 
+
+personDetails = 'salman'
+personDetails = 21
+
